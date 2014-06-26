323f453a3bd46d55a979294cdf8135735065c3aa
Gustavo Isensee <gustavo.isensee@gmail.com>
2014-06-26 16:58:29 -0300

    update the layout

--- js/scope.js
+++ js/scope.js
@@ -8,32 +8,35 @@ function Scope() {
 	var current = "home",
 
 		modulo = angular
-			.module('WebSite', [])
-			.directive('menu', function() {
-			    return {
-			      	template: '<li class="current">'+
-						'<span data-to="home" class="menu">{{language.menuHome}}</span>'+
-						'</li><li><span data-to="project" class="menu">{{language.menuProject}}</span>'+
-						'</li><li><span data-to="contact" class="menu">{{language.menuContact}}</span>'+
-						'</li><li><span>lang: { </span>'+
-						'<span class=\'{{language.lang == "ptBR" ? "underline" : ""}} \''+
-						'  ng-click=\'alterLang("ptBR")\'>{{language.menuLangPtBR}}</span> <span>, </span>'+
-						'<span class=\'{{language.lang == "enUS" ? "underline" : ""}} \''+
-						'  ng-click=\'alterLang("enUS")\'>{{language.menuLangEnUS}}</span> <span> }</span></li>' +
-						'<div class="contact">' +
-						'<a class="gplus-icon" href="https://plus.google.com/109325404047497404656/about"' +
-						'target="_blank" title="Google+ - Gustavo Isensee"></a>' +
-                        '<a class="facebook-icon" href="http://www.facebook.com/profile.php?id=100000394403997"' +
-                		'target="_blank" title="Facebook - Gustavo Isensee"></a>'+
-                        '<a class="linkedin-icon" href="http://www.linkedin.com/pub/gustavo-isensee/3a/93b/752"' +
-                		'target="_blank" title="LinkedIn - Gustavo Isensee"></a>' +
-                		'<a class="twitter-icon" href="http://twitter.com/gustavoisensee" target="_blank"' +
-                		'title="Twitter - @gustavoisensee"></a>' +
-						'<a class="github-icon" href="https://github.com/gustavoisensee" target="_blank" ' +
-                		'title="Github - gustavoisensee"></a></div>'
-			    };
-			}
-		),
+		.module('WebSite', [])
+		.directive('menu', function() {
+		    return {
+		      	template: '<li class="current">'+
+					'<span data-to="home" class="menu">{{language.menuHome}}</span>'+
+					'</li><li><span data-to="project" class="menu">{{language.menuProject}}</span>'+
+					'</li><li><span data-to="contact" class="menu">{{language.menuContact}}</span>'+
+					'</li><li><span>lang: { </span>'+
+					'<span class=\'{{language.lang == "ptBR" ? "underline" : ""}} \''+
+					'  ng-click=\'alterLang("ptBR")\'>{{language.menuLangPtBR}}</span> <span>, </span>'+
+					'<span class=\'{{language.lang == "enUS" ? "underline" : ""}} \''+
+					'  ng-click=\'alterLang("enUS")\'>{{language.menuLangEnUS}}</span> <span> }</span></li>'
+		    };
+		})
+		.directive('contact', function() {
+		    return {
+		      	template: '<div class="contact">' +
+					'<a class="gplus-icon" href="https://plus.google.com/109325404047497404656/about"' +
+					'target="_blank" title="Google+ - Gustavo Isensee"></a>' +
+                    '<a class="facebook-icon" href="http://www.facebook.com/profile.php?id=100000394403997"' +
+            		'target="_blank" title="Facebook - Gustavo Isensee"></a>'+
+                    '<a class="linkedin-icon" href="http://www.linkedin.com/pub/gustavo-isensee/3a/93b/752"' +
+            		'target="_blank" title="LinkedIn - Gustavo Isensee"></a>' +
+            		'<a class="twitter-icon" href="http://twitter.com/gustavoisensee" target="_blank"' +
+            		'title="Twitter - @gustavoisensee"></a>' +
+					'<a class="github-icon" href="https://github.com/gustavoisensee" target="_blank" ' +
+            		'title="Github - gustavoisensee"></a></div>'
+		    };
+		}),
 
 		Controller = function ($scope) {
 			$scope.contact = {
