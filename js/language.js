8995ec5b44ec447c34c3b8ad4f3b113841554ac7
Gustavo Isensee <gustavo.isensee@gmail.com>
2014-07-02 17:12:52 -0300

    updated code
    Added new icons
    css updated
    added language to the title of legends the project page

--- js/language.js
+++ js/language.js
@@ -21,6 +21,8 @@ function LangPage () {
 		developer: 'Desenvolvedor Web',
 		myDescription: "Eu sou Gustavo, tenho " + calculateAge() + " anos de idade, formado como técnico de programação pelo colégio SENAI. Atualmente estou cursando a 8ª de Sistemas de Informação na UNISUL. Trabalhei em duas empresas privadas, particípo de projetos open source no GitHub, sou um grande entusiasta Javascript, certificado MCSD pela Microsoft e atualmente estou trabalhando no LabTrans, Laboratório de Transportes e Logística da Universidade Federal de Santa Catarina, Brasil.",
 
+		legendTitleCurrentProjet: 'Projeto corrente',
+		legendTitleFinishedProjet: 'Projeto finalizado',
 		goToProject: 'Ir para Projetos',
 		project: 'Projetos',
 
@@ -48,6 +50,8 @@ function LangPage () {
 			developer: 'Web Developer',
 			myDescription: "I am Gustavo, I'm " + calculateAge() + " years old, Graduated in technical programming at the college SENAI. I'm currently studying the 8th of Information Systems at UNISUL. I worked in two private companies, participate in open source projects on GitHub, I'm a great enthusiast Javascript, MCSD certified by Microsoft and I'm currently working on LabTrans, Transportation and Logistics Laboratory of the Federal University of Santa Catarina, Brazil.",
 
+			legendTitleCurrentProjet: 'Current project',
+			legendTitleFinishedProjet: 'Finished project',
 			goToProject: 'Go to Projects',
 			project: 'Projects',
 
