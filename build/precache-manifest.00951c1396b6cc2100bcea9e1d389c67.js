84196b5b26b9dda8d811519dd0d1a76702e4800c
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-02-20 18:32:46 +0100

    remove build from gitignore to deploy in gh-pages

---
+++ build/precache-manifest.00951c1396b6cc2100bcea9e1d389c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98ebb9589e47d7540c9ed27f2c513cd",
+    "url": "/static/media/profile.e98ebb95.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "27913ba41b87586d396d",
+    "url": "/static/js/main.27913ba4.chunk.js"
+  },
+  {
+    "revision": "a26d512a013bf750007c",
+    "url": "/static/js/2.a26d512a.chunk.js"
+  },
+  {
+    "revision": "27913ba41b87586d396d",
+    "url": "/static/css/main.a42cb799.chunk.css"
+  },
+  {
+    "revision": "9550f3fb882534e62b78990c37da32d4",
+    "url": "/index.html"
+  }
+];
