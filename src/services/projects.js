3f6d0ec01f87c05317739ae0ea291f74babac801
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-10-29 16:29:17 +0100

    fix: add Authorization header

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,7 +1,9 @@
 export const getProjects = async() => {
+  const token = process.env.REACT_APP_GITHUB_TOKEN;
   const url = 'https://api.github.com/users/gustavoisensee/repos?sort=updated';
   const options = {
     headers: {
+      Authorization: `token ${token}`,
       Accept: 'application/vnd.github.v3+json',
     }
   }
