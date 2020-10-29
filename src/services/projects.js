d947d7f70c42571233f0455a5d2986a8a766744a
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-10-29 16:21:55 +0100

    fix: add accept headers

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,11 +1,17 @@
 export const getProjects = async() => {
   const url = 'https://api.github.com/users/gustavoisensee/repos?sort=updated';
-    try {
-      const response = await fetch(url);
-      if (response.ok) return response.json();
-
-      return [];
-    } catch (err) {
-      return [];
+  const options = {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
     }
+  }
+
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) return response.json();
+
+    return [];
+  } catch (err) {
+    return [];
+  }
 };
