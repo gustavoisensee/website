f37c36fb9d7aecb5ebf0a5a9a4058eadc3ba6e0c
Gustavo Isensee <gustavo.isensee@gmail.com>
2022-06-29 17:48:01 +0200

    feat: add blog post margin and italic font

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import dayjs from '../../helpers/dayjs';
 
 const Post = ({ post }) => (
-  <div>
+  <div className='mb-4'>
     <a className='underline' href={post.url} target='_blank' rel='noopener noreferrer'>
       <h3 className='text-xl font-semibold'>{post.title}</h3>
     </a>
     {post.description && <p>{post.description}</p>}
-    <span>{dayjs(post.created_at).format('LLL')}</span>
+    <span className='italic'>{dayjs(post.created_at).format('LLL')}</span>
   </div>
 );
 
