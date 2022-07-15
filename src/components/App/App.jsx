3d3f1bfbdd4f7044f03c176f2edfc3301e8080ad
Gustavo Isensee <gustavo.isensee@gmail.com>
2022-07-15 23:37:41 +0200

    fix: update margin top

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import Content from '../Content';
 import './styles.scss';
 
 const App = () => (
-  <div className='w-full max-w-lg p-4'>
+  <div className='w-full max-w-lg py-8 px-4'>
     <SideMenu />
     <Header />
     <Content />
