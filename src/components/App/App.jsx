9004be3f68ea2e0ee05ee5caa604dff1dbc76878
Gustavo Isensee <gustavo.isensee@gmail.com>
2023-06-20 16:46:32 +0200

    Create new sidebar menu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../Header';
-import SideMenu from '../SideMenu';
+import SideMenuNew from '../SideMenuNew/SideMenuNew';
 import Content from '../Content';
 import './styles.scss';
 
 const App = () => (
   <div className='w-full max-w-2xl py-8 px-4'>
-    <SideMenu />
+    <SideMenuNew />
     <Header />
     <Content />
   </div>
--- src/components/Pill/Pill.jsx
+++ src/components/Pill/Pill.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const Pill = ({ children, className }) => (
   <div
     className={`${className} rounded-2xl shadow-md max-w-fit px-3 py-2 mr-2 mb-2 flex align-middle justify-center`}
-    // style={{ boxShadow: '0px 2px 4px 0px rgba(0, 0, 0, 0.14)' }}
   >
-    <span className='text-gray-800 text-sm font-medium'>{children}</span>
+    <span className='text-gray-700 text-sm font-medium'>{children}</span>
   </div>
 );
 
