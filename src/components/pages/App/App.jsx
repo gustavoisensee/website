25a469415d6ce1fbeaffb2aecda9ca4e5e6feb09
Gustavo Isensee <gustavo.isensee@gmail.com>
2023-06-21 23:34:56 +0200

    fix github actions

--- src/components/pages/App/App.jsx
+++ src/components/pages/App/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header';
 import SideMenu from './components/SideMenu';
 import Content from './components/Content';
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: mode === 'development' ? [react()] : [],
-  esbuild: {
-    jsxInject: `import * as React from 'react'`,
-  }
-}))
+export default defineConfig({
+  plugins: [react()]
+})
+
