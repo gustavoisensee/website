07da534015e3d576336fa6f852f6dee2719bbfb6
Gustavo Isensee <gustavo.isensee@gmail.com>
2023-06-21 23:17:20 +0200

    fix for github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'classic'
-  })]
-})
+export default defineConfig(({ mode }) => ({
+  plugins: mode === 'development' ? [react()] : [],
+  esbuild: {
+    jsxInject: `import * as React from 'react'`,
+  }
+}))
