65773d5db18c395e0854240a7cd1c110d406baa1
Gustavo Isensee <gustavo.isensee@gmail.com>
2023-12-22 11:59:00 +0100

    fix: menu overlapping

--- src/components/app/SideMenu/SideMenu.jsx
+++ src/components/app/SideMenu/SideMenu.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cn from 'classnames';
+
 import Footer from './components/Footer';
 import SideMenuButton from './components/SideMenuButton';
 import LanguageSwitcher from './components/LanguageSwitcher';
@@ -10,11 +12,22 @@ const SideMenu = () => {
 
   return (
     <div>
-      <div className='fixed right-0 top-0 bg-gray-100 text-gray-800 h-fit rounded-bl-md'>
+      <div
+        className={cn(
+          'fixed right-0 top-0 bg-gray-100',
+          'text-gray-800 h-fit rounded-bl-md z-10'
+        )}
+      >
         <SideMenuButton />
       </div>
 
-      <div className='fixed shadow-lg top-0 right-0 translate-x-56 h-screen sidebar bg-slate-50 text-gray-900 py-7 px-2 inset-y-0 transform transition duration-200 ease-in-out z-10'>
+      <div
+        className={cn(
+          'fixed shadow-lg top-0 right-0 translate-x-56 h-screen',
+          'sidebar bg-slate-50 text-gray-900 py-7 px-2',
+          'inset-y-0 transform transition duration-200 ease-in-out z-10'
+        )}
+      >
         <Menu />
         <LanguageSwitcher />
         <Footer />
