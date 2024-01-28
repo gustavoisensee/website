d5735f3a356b656772ebb784e515ee47965cfe9f
Gustavo Isensee <gustavo.isensee@gmail.com>
2024-01-28 23:59:54 +0100

    fix: dayjs format

--- src/components/projects/ProjectCard/ProjectCard.tsx
+++ src/components/projects/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { ProjectType } from '../../../types';
 
+dayjs.extend(localizedFormat);
 
 type Props = {
   project: ProjectType
