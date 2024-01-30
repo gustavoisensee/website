b6827190d27bce8900720d306799153606485525
Gustavo Isensee <gustavo.isensee@gmail.com>
2024-01-31 00:25:29 +0100

    feat: add more assets

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Home page', async ({ page }) => {
-  await page.goto('https://gustavoisensee.com/');
-  await expect(page.getByRole('heading', { name: 'Gustavo Isensee' })).toBeVisible();
+const url =
+  process.env.NODE_ENV === "dev"
+    ? "http://localhost:5173/"
+    : "https://gustavoisensee.com/";
 
-  await expect(page.getByRole('heading', { name: 'Tech knowledge' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'About me' })).toBeVisible();
+test("Home page", async ({ page }) => {
+  await page.goto(url);
+  await expect(
+    page.getByRole("heading", { name: "Gustavo Isensee" })
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole("heading", { name: "Tech knowledge" })
+  ).toBeVisible();
+  await expect(page.getByRole("heading", { name: "About me" })).toBeVisible();
+
+  await page.getByRole("button", { name: "Show all" }).click();
+  await expect(page.getByText("SQL Server")).toBeVisible();
 });
