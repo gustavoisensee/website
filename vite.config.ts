56deda4a13abe6ef2803ee69b610ada3e988aac8
Gustavo Isensee <gustavo.isensee@gmail.com>
2024-01-31 00:17:08 +0100

    fix: vitest config exclude playwright tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  
+  plugins: [react()],
+  // @ts-ignore
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      'tests/**'
+    ]
+  },
 })
